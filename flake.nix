27e789d6e609f438c02fe70153f7452832c9293e
Anillc <i@anillc.cn>
2023-02-22 23:50:13 +0800

    migrate to c

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    packages.default = pkgs.stdenv.mkDerivation {
+      name = "remu";
+      src = ./.;
+    };
+  });
+}
