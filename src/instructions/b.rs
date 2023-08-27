ae2328dbaf6c171ed71998ebb853350c07a52d72
Anillc <i@anillc.cn>
2023-08-27 18:10:56 +0800

    add BLT BGE BLT BGEU

--- src/instructions/b.rs
+++ src/instructions/b.rs
@@ -29,5 +29,61 @@ pub(crate) fn b_instructions() -> Vec<Instructor> {
         _ => unreachable!(),
       }
     },
+
+    Instructor {
+      name: "BLT",
+      opcode: 0b1100011,
+      funct: Funct::B(0b100),
+      run: |inst, cpu| match inst {
+        Instruction::B { imm, rs2, rs1, funct3: _, opcode: _ } => {
+          if (cpu.regs[rs1] as i64) < (cpu.regs[rs2] as i64) {
+            cpu.pc = cpu.pc.wrapping_add(imm as u64);
+          }
+        },
+        _ => unreachable!(),
+      }
+    },
+
+    Instructor {
+      name: "BGE",
+      opcode: 0b1100011,
+      funct: Funct::B(0b101),
+      run: |inst, cpu| match inst {
+        Instruction::B { imm, rs2, rs1, funct3: _, opcode: _ } => {
+          if (cpu.regs[rs1] as i64) > (cpu.regs[rs2] as i64) {
+            cpu.pc = cpu.pc.wrapping_add(imm as u64);
+          }
+        },
+        _ => unreachable!(),
+      }
+    },
+
+    Instructor {
+      name: "BLTU",
+      opcode: 0b1100011,
+      funct: Funct::B(0b110),
+      run: |inst, cpu| match inst {
+        Instruction::B { imm, rs2, rs1, funct3: _, opcode: _ } => {
+          if cpu.regs[rs1] < cpu.regs[rs2] {
+            cpu.pc = cpu.pc.wrapping_add(imm as u64);
+          }
+        },
+        _ => unreachable!(),
+      }
+    },
+
+    Instructor {
+      name: "BGEU",
+      opcode: 0b1100011,
+      funct: Funct::B(0b111),
+      run: |inst, cpu| match inst {
+        Instruction::B { imm, rs2, rs1, funct3: _, opcode: _ } => {
+          if cpu.regs[rs1] > cpu.regs[rs2] {
+            cpu.pc = cpu.pc.wrapping_add(imm as u64);
+          }
+        },
+        _ => unreachable!(),
+      }
+    },
   ])
 }
