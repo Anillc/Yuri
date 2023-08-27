0ff9b4c1924c0d9e2560ba7bb0fe015178844fef
Anillc <i@anillc.cn>
2023-08-27 18:03:19 +0800

    add BEQ and BNE

--- src/instructions/b.rs
+++ src/instructions/b.rs
@@ -1,5 +1,33 @@
-use super::Instructor;
+use super::{Instructor, Funct, Instruction};
 
 pub(crate) fn b_instructions() -> Vec<Instructor> {
-  vec![]
+  Vec::from([
+    Instructor {
+      name: "BEQ",
+      opcode: 0b1100011,
+      funct: Funct::B(0b000),
+      run: |inst, cpu| match inst {
+        Instruction::B { imm, rs2, rs1, funct3: _, opcode: _ } => {
+          if cpu.regs[rs1] == cpu.regs[rs2] {
+            cpu.pc = cpu.pc.wrapping_add(imm as u64);
+          }
+        },
+        _ => unreachable!(),
+      }
+    },
+
+    Instructor {
+      name: "BNE",
+      opcode: 0b1100011,
+      funct: Funct::B(0b001),
+      run: |inst, cpu| match inst {
+        Instruction::B { imm, rs2, rs1, funct3: _, opcode: _ } => {
+          if cpu.regs[rs1] != cpu.regs[rs2] {
+            cpu.pc = cpu.pc.wrapping_add(imm as u64);
+          }
+        },
+        _ => unreachable!(),
+      }
+    },
+  ])
 }
