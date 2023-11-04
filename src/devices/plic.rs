51a779b5779710723ddada1c62debd356cc15950
Anillc <i@anillc.cn>
2023-11-04 13:41:20 +0800

    make plic extensable

--- src/devices/plic.rs
+++ src/devices/plic.rs
@@ -11,34 +11,47 @@ pub(crate) const PLIC_SOURCE_PRIORITY_END: u64 = PLIC_START + 0x000FFF;
 pub(crate) const PLIC_PENDING_START: u64 = PLIC_START + 0x001000;
 pub(crate) const PLIC_PENDING_END: u64 = PLIC_START + 0x00107F;
 
-pub(crate) const PLIC_SOURCE_ENABLE_0_START: u64 = PLIC_START + 0x002000;
-pub(crate) const PLIC_SOURCE_ENABLE_0_END: u64 = PLIC_SOURCE_ENABLE_0_START + 128 - 1;
+pub(crate) const PLIC_SOURCE_ENABLE_START: u64 = PLIC_START + 0x002000;
+pub(crate) const PLIC_SOURCE_ENABLE_END: u64 = PLIC_START + 0x1F1FFF;
 
-pub(crate) const PLIC_SOURCE_ENABLE_1_START: u64 = PLIC_START + 0x002080;
-pub(crate) const PLIC_SOURCE_ENABLE_1_END: u64 = PLIC_SOURCE_ENABLE_1_START + 128 - 1;
+pub(crate) const PLIC_THRESHOLD_CLIAM_COMPLETE_START: u64 = PLIC_START + 0x200000;
+pub(crate) const PLIC_THRESHOLD_CLIAM_COMPLETE_END: u64 = PLIC_START + 0x3FFFFFF;
 
-pub(crate) const PLIC_THRESHOLD0_START: u64 = PLIC_START + 0x200000;
-pub(crate) const PLIC_THRESHOLD0_END: u64 = PLIC_THRESHOLD0_START + 4 - 1;
+// TODO: hart count
+const HART_COUNT: usize = 1;
+const INTERRUPT_COUNT: usize = 64;
 
-pub(crate) const PLIC_CLIAMCOMPLETE0_START: u64 = PLIC_START + 0x200004;
-pub(crate) const PLIC_CLIAMCOMPLETE0_END: u64 = PLIC_CLIAMCOMPLETE0_START + 4 - 1;
+#[derive(Debug, Clone, Copy)]
+pub(crate) struct Pair<T: Clone + Copy> {
+  machine: T,
+  supervisor: T,
+}
 
-pub(crate) const PLIC_THRESHOLD1_START: u64 = PLIC_START + 0x201000;
-pub(crate) const PLIC_THRESHOLD1_END: u64 = PLIC_THRESHOLD1_START + 4 - 1;
+impl<T: Clone + Copy> Pair<T> {
+  pub(crate) fn at(&self, index: usize) -> &T {
+    match index {
+      0 => &self.machine,
+      1 => &self.supervisor,
+      _ => unreachable!(),
+    }
+  }
 
-pub(crate) const PLIC_CLIAMCOMPLETE1_START: u64 = PLIC_START + 0x201004;
-pub(crate) const PLIC_CLIAMCOMPLETE1_END: u64 = PLIC_CLIAMCOMPLETE1_START + 4 - 1;
-
-const CONTEXT_LENGTH: usize = 2;
+  pub(crate) fn at_mut(&mut self, index: usize) -> &mut T {
+    match index {
+      0 => &mut self.machine,
+      1 => &mut self.supervisor,
+      _ => unreachable!(),
+    }
+  }
+}
 
 #[derive(Debug)]
 pub(crate) struct Plic {
   priorities: [u32; 1023],
   pending: [u32; 32],
-  // TODO: if supports multiple harts, make sure there are hart * 2 contexts
-  enable: [[u32; 32]; CONTEXT_LENGTH],
-  threshold: [u32; CONTEXT_LENGTH],
-  claimed: [[bool; 1024]; CONTEXT_LENGTH],
+  enable: [Pair<[u32; 32]>; HART_COUNT * 2],
+  threshold: [Pair<u32>; HART_COUNT * 2],
+  claimed: [Pair<[bool; 1024]>; HART_COUNT * 2],
   update: bool,
 }
 
@@ -47,9 +60,9 @@ impl Plic {
     Plic {
       priorities: [0; 1023],
       pending: [0; 32],
-      enable: [[0; 32]; CONTEXT_LENGTH],
-      threshold: [0; CONTEXT_LENGTH],
-      claimed: [[false; 1024]; CONTEXT_LENGTH],
+      enable: [Pair { machine: [0; 32], supervisor: [0; 32] }; HART_COUNT * 2],
+      threshold: [Pair { machine: 0, supervisor: 0 }; HART_COUNT * 2],
+      claimed: [Pair { machine: [false; 1024], supervisor: [false; 1024] }; HART_COUNT * 2],
       update: false,
     }
   }
@@ -62,7 +75,7 @@ impl Plic {
   }
 
   fn complete(&mut self, context: usize, irq: u32) {
-    self.claimed[context][irq as usize] = false;
+    self.claimed[context / 2].at_mut(context % 2)[irq as usize] = false;
   }
 
   fn claim(&mut self, context: usize) -> u32 {
@@ -70,20 +83,22 @@ impl Plic {
     let index = (irq / 8) as usize;
     let offset = irq % 8;
     self.pending[index] &= !(1 << offset);
-    self.claimed[context][irq as usize] = true;
+    self.claimed[context / 2].at_mut(context % 2)[irq as usize] = true;
     irq
   }
 
   fn highest_irq(&mut self, context: usize) -> u32 {
     let mut irq: u32 = 0;
     let mut priority = 0;
-    for i in 1..1024 {
+    for i in 1..INTERRUPT_COUNT {
       let index = i / 32;
       let offset = i % 32;
-      if self.enable[context][index] & (1 << offset) != 0
+      let hart = context / 2;
+      let mode = context % 2;
+      if self.enable[hart].at(mode)[index] & (1 << offset) != 0
         && self.pending[index] & (1 << offset) != 0
-        && !self.claimed[context][index]
-        && self.priorities[i] > self.threshold[context]
+        && !self.claimed[hart].at(mode)[index]
+        && self.priorities[i] > *self.threshold[hart].at(mode)
         && self.priorities[i] > priority {
           irq = i as u32;
           priority = self.priorities[i];
@@ -99,15 +114,10 @@ impl Device for Plic {
   fn step(&mut self, _bus: &mut Bus, hart: &mut Hart) {
     if self.update {
       self.update = false;
-      for context in 0..CONTEXT_LENGTH {
+      for context in 0..HART_COUNT {
         let ip = if self.highest_irq(context) != 0 { 1 } else { 0 };
-        if context % 2 == 0 {
-          // M-mode
-          hart.csr.write_mip_meip(ip);
-        } else {
-          // S-mode
-          hart.csr.write_mip_seip(ip);
-        }
+        hart.csr.write_mip_meip(ip);
+        hart.csr.write_mip_seip(ip);
       }
     }
   }
@@ -134,54 +144,60 @@ impl Device for Plic {
   }
 
   fn read32(&mut self, address: u64) -> Result<u32, Exception> {
+    if address % 4 != 0 { return Err(Exception::LoadAddressMisaligned(address)); }
     match address {
       PLIC_SOURCE_PRIORITY_START..=PLIC_SOURCE_PRIORITY_END =>
         Ok(self.priorities[(address - PLIC_SOURCE_PRIORITY_START) as usize]),
       PLIC_PENDING_START..=PLIC_PENDING_END =>
         Ok(self.pending[(address - PLIC_PENDING_START) as usize]),
-      PLIC_SOURCE_ENABLE_0_START..=PLIC_SOURCE_ENABLE_0_END =>
-        Ok(self.enable[0][(address - PLIC_SOURCE_ENABLE_0_START) as usize]),
-      PLIC_SOURCE_ENABLE_1_START..=PLIC_SOURCE_ENABLE_1_END =>
-        Ok(self.enable[1][(address - PLIC_SOURCE_ENABLE_1_START) as usize]),
-      PLIC_THRESHOLD0_START..=PLIC_THRESHOLD0_END => Ok(self.threshold[0]),
-      PLIC_THRESHOLD1_START..=PLIC_THRESHOLD1_END => Ok(self.threshold[1]),
-      PLIC_CLIAMCOMPLETE0_START..=PLIC_CLIAMCOMPLETE0_END => {
-        let irq = self.claim(0);
-        self.update = true;
-        Ok(irq)
+      PLIC_SOURCE_ENABLE_START..=PLIC_SOURCE_ENABLE_END => {
+        let offset = (address - PLIC_SOURCE_ENABLE_START) as usize;
+        let context = offset / 0x80;
+        let item = offset % 0x80;
+        Ok(self.enable[context / 2].at(context % 2)[item])
       },
-      PLIC_CLIAMCOMPLETE1_START..=PLIC_CLIAMCOMPLETE1_END => {
-        let irq = self.claim(1);
-        self.update = true;
-        Ok(irq)
+      PLIC_THRESHOLD_CLIAM_COMPLETE_START..=PLIC_THRESHOLD_CLIAM_COMPLETE_END => {
+        let offset = (address - PLIC_THRESHOLD_CLIAM_COMPLETE_START) as usize;
+        let context = offset / 0x1000;
+        let item = offset % 0x1000;
+        match item {
+          // threshold
+          0 => Ok(*self.threshold[context / 2].at(context % 2)),
+          // claim
+          1 => {
+            let irq = self.claim(context);
+            self.update = true;
+            Ok(irq)
+          },
+          _ => Err(Exception::LoadAccessFault(address)),
+        }
       },
       _ => Err(Exception::LoadAccessFault(address)),
     }
   }
 
   fn write32(&mut self, address: u64, data: u32) -> Result<(), Exception> {
+    if address % 4 != 0 { return Err(Exception::StoreAMOAddressMisaligned(address)); }
     match address {
       PLIC_SOURCE_PRIORITY_START..=PLIC_SOURCE_PRIORITY_END =>
         self.priorities[(address - PLIC_SOURCE_PRIORITY_START) as usize] = data,
-      PLIC_SOURCE_ENABLE_0_START..=PLIC_SOURCE_ENABLE_0_END =>
-        self.enable[0][(address - PLIC_SOURCE_ENABLE_0_START) as usize] = data,
-      PLIC_SOURCE_ENABLE_1_START..=PLIC_SOURCE_ENABLE_1_END =>
-        self.enable[1][(address - PLIC_SOURCE_ENABLE_1_START) as usize] = data,
-      PLIC_THRESHOLD0_START..=PLIC_THRESHOLD0_END => {
-        self.threshold[0] = data;
-        self.update = true;
+      PLIC_SOURCE_ENABLE_START..=PLIC_SOURCE_ENABLE_END => {
+        let offset = (address - PLIC_SOURCE_ENABLE_START) as usize;
+        let context = offset / 0x80;
+        let item = offset % 0x80;
+        self.enable[context / 2].at_mut(context % 2)[item] = data;
       },
-      PLIC_THRESHOLD1_START..=PLIC_THRESHOLD1_END => {
-        self.threshold[1] = data;
-        self.update = true;
-      },
-      PLIC_CLIAMCOMPLETE0_START..=PLIC_CLIAMCOMPLETE0_END => {
-        self.complete(0, data);
-        self.update = true;
-      },
-      PLIC_CLIAMCOMPLETE1_START..=PLIC_CLIAMCOMPLETE1_END => {
-        self.complete(1, data);
-        self.update = true;
+      PLIC_THRESHOLD_CLIAM_COMPLETE_START..=PLIC_THRESHOLD_CLIAM_COMPLETE_END => {
+        let offset = (address - PLIC_THRESHOLD_CLIAM_COMPLETE_START) as usize;
+        let context = offset / 0x1000;
+        let item = offset % 0x1000;
+        match item {
+          // threshold
+          0 => *self.threshold[context / 2].at_mut(context % 2) = data,
+          // complete
+          1 => self.complete(context, data),
+          _ => return Err(Exception::LoadAccessFault(address)),
+        };
       },
       _ => return Err(Exception::StoreAMOAccessFault(address)),
     };
--- src/hart.rs
+++ src/hart.rs
@@ -130,6 +130,7 @@ impl Hart {
     let trap_value = match trap {
         Trap::Exception(Exception::InstructionAccessFault(value))
       | Trap::Exception(Exception::Breakpoint(value))
+      | Trap::Exception(Exception::LoadAddressMisaligned(value))
       | Trap::Exception(Exception::LoadAccessFault(value))
       | Trap::Exception(Exception::StoreAMOAddressMisaligned(value))
       | Trap::Exception(Exception::StoreAMOAccessFault(value))
