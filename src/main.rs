37047b7972d94fb4d5184b29c09c3d9dd01eb56d
Anillc <i@anillc.cn>
2023-11-07 00:17:37 +0800

    support htif

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![feature(atomic_from_ptr)]
 #![feature(try_blocks)]
+use std::io::Read;
+
 use cpu::Cpu;
+use utils::channel::channel;
 
 mod cpu;
 mod hart;
@@ -13,13 +16,13 @@ mod trap;
 mod devices;
 
 fn main() {
-  let (mut cpu, controller) = Cpu::new();
+  let (mut cpu, _controller) = Cpu::new();
+  let (sender, receiver) = channel::<i32>();
   std::thread::spawn(move || {
-    loop {
-      let r = controller.uart_receiver.recv();
-      print!("{}", char::from(r));
+    for i in std::io::stdin().bytes() {
+      sender.send(i.unwrap() as i32);
     }
+    sender.send(-1);
   });
-  cpu.load_elf("fw_payload.elf");
-  cpu.run();
+  cpu.run_htif("tests/amtest-yuri.elf", receiver);
 }
