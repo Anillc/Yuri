9910e614e95675e533827b7b56b781386494a321
Anillc <i@anillc.cn>
2023-11-07 01:11:27 +0800

    use run_htif in tests

--- src/cpu.rs
+++ src/cpu.rs
@@ -39,8 +39,8 @@ impl Cpu {
     }
   }
 
-  pub(crate) fn run_htif(&mut self, filepath: PathBuf, stdin: Receiver<i32>) {
-    let file = fs::read(filepath).unwrap();
+  pub(crate) fn run_htif(&mut self, file: PathBuf, stdin: Receiver<i32>) {
+    let file = fs::read(file).unwrap();
     let elf = ElfBytes::<LittleEndian>::minimal_parse(&file).unwrap();
     for segment in elf.segments().unwrap() {
       for i in 0..segment.p_filesz {
@@ -70,6 +70,7 @@ impl Cpu {
         let cmd = fromvm << 8 >> 56;
         let data = fromvm << 16 >> 16;
         match dev {
+          0 if cmd == 0 && data == 1 => break,
           1 if cmd == 0 => self.bus.write64(fromhost, (1 << 56) | (stdin.recv() as u64)).unwrap(),
           1 if cmd == 1 => print!("{}", char::from_u32(data as u32).unwrap()),
           _ => panic!("terminated with code {:x}", fromvm),
@@ -82,53 +83,18 @@ impl Cpu {
 #[cfg(test)]
 mod tests {
   use std::fs;
-  use elf::{ElfBytes, endian::LittleEndian};
-  use crate::devices::Device;
+  use crate::utils::channel::channel;
   use super::Cpu;
 
   #[test]
   fn riscv_tests() {
     let dir = fs::read_dir("tests").unwrap();
     for file in dir {
-      let file = file.unwrap();
+      let (_, htif_receiver) = channel::<i32>();
+      let file = file.unwrap().path();
       let (mut cpu, _) = Cpu::new();
-      cpu.run_test(file.path().to_str().unwrap());
-    }
-  }
-
-  impl Cpu {
-    pub(crate) fn run_test(&mut self, filepath: &str) {
-      let file = fs::read(filepath).unwrap();
-      let elf = ElfBytes::<LittleEndian>::minimal_parse(&file).unwrap();
-      for segment in elf.segments().unwrap() {
-        for i in 0..segment.p_filesz {
-          if segment.p_type != elf::abi::PT_LOAD { continue; }
-          self.bus.write8(segment.p_paddr + i, file[(segment.p_offset + i) as usize]).unwrap();
-        }
-      }
-      self.hart.pc = elf.ehdr.e_entry;
-      let (parsing_table, string_table) = elf.symbol_table().unwrap().unwrap();
-      let tohost = parsing_table.iter().find(|symbol|
-        string_table.get(symbol.st_name as usize).unwrap() == "tohost")
-        .unwrap().st_value;
-
-      let mut bus = self.bus.clone();
-      loop {
-        self.hart.step(&mut self.mmu);
-        self.bus.step(&mut bus, &mut self.hart);
-        let fromvm = self.bus.read64(tohost).unwrap();
-        if fromvm != 0 {
-          self.bus.write64(tohost, 0).unwrap();
-          if fromvm >> 32 == 0x01010000 {
-            print!("{}", char::from_u32(fromvm as u32).unwrap());
-          } else if fromvm == 1 {
-            println!("'{}' passed", filepath);
-            break;
-          } else {
-            panic!("'{}' terminated with code {:x}", filepath, fromvm);
-          }
-        }
-      }
+      cpu.run_htif(file.clone(), htif_receiver);
+      println!("'{}' passed", file.to_str().unwrap());
     }
   }
 }
