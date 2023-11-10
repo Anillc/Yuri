b13f9413015ccba6fddc611babb2120534813278
Anillc <i@anillc.cn>
2023-11-10 13:23:13 +0800

    add vga device

--- src/devices/ysyx.rs
+++ src/devices/ysyx.rs
@@ -5,16 +5,33 @@ use crate::{device_atomic, hart::Hart, trap::Exception};
 use super::{Device, bus::Bus};
 
 pub(crate) const YSYX_START: u64 = 0x20000000;
-pub(crate) const YSYX_END: u64 = YSYX_START + 0x01000000 - 1;
+pub(crate) const YSYX_END: u64 = YSYX_START + 0x10000000 - 1;
 
-pub(crate) const YSYX_TIME: u64 = YSYX_START;
+const VGA_WIDTH: usize = 800;
+const VGA_HEIGHT: usize = 600;
+
+
+const YSYX_TIME: u64 = YSYX_START;
+
+const YSYX_VGACTL_ADDR_LOW: u64 = YSYX_START + 0x100;
+// sync
+const YSYX_VGACTL_ADDR_HIGH: u64 = YSYX_START + 0x100 + 4;
+
+const YSYX_FB_START: u64 = YSYX_START + 0x01000000;
+const YSYX_FB_END: u64 = YSYX_FB_START + ((VGA_WIDTH * VGA_HEIGHT * 4) as u64) - 1;
 
 #[derive(Debug)]
-pub(crate) struct Ysyx {}
+pub(crate) struct Ysyx {
+  vgactl: [u32; 2],
+  vmem: [u32; VGA_WIDTH * VGA_HEIGHT],
+}
 
 impl Ysyx {
   pub(crate) fn new() -> Ysyx {
-    Ysyx {}
+    Ysyx {
+      vgactl: [((VGA_WIDTH << 16) | VGA_HEIGHT) as u32, 0],
+      vmem: [0; VGA_WIDTH * VGA_HEIGHT],
+    }
   }
 }
 
@@ -32,7 +49,12 @@ impl Device for Ysyx {
   }
 
   fn read32(&mut self, address: u64) -> Result<u32, Exception> {
-    Err(Exception::LoadAccessFault(address))
+    match address {
+      YSYX_VGACTL_ADDR_LOW => Ok(self.vgactl[0]),
+      YSYX_VGACTL_ADDR_HIGH => Ok(self.vgactl[1]),
+      YSYX_FB_START..=YSYX_FB_END => Ok(self.vmem[((address - YSYX_FB_START) / 4) as usize]),
+      _ => Err(Exception::LoadAccessFault(address)),
+    }
   }
 
   fn read64(&mut self, address: u64) -> Result<u64, Exception> {
@@ -50,8 +72,16 @@ impl Device for Ysyx {
     Err(Exception::StoreAMOAccessFault(address))
   }
 
-  fn write32(&mut self, address: u64, _data: u32) -> Result<(), Exception> {
-    Err(Exception::StoreAMOAccessFault(address))
+  fn write32(&mut self, address: u64, data: u32) -> Result<(), Exception> {
+    match address {
+      YSYX_VGACTL_ADDR_HIGH => {
+        // TODO
+        dbg!("sync");
+      },
+      YSYX_FB_START..=YSYX_FB_END => self.vmem[((address - YSYX_FB_START) / 4) as usize] = data,
+      _ => return Err(Exception::StoreAMOAccessFault(address)),
+    }
+    Ok(())
   }
 
   fn write64(&mut self, address: u64, _data: u64) -> Result<(), Exception> {
