bf6a577b1186cac2513cb8cfee8329ea1b66df24
Anillc <i@anillc.cn>
2023-11-10 14:05:04 +0800

    add flake

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Flake utils demo";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [];
+        buildInputs = with pkgs; [];
+      };
+    });
+}
