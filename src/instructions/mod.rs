16c52f8951493925ff6f9498617d90325c07049f
Anillc <i@anillc.cn>
2023-11-11 22:34:34 +0800

    optimize parsing

--- src/instructions/mod.rs
+++ src/instructions/mod.rs
@@ -1,6 +1,6 @@
-use std::collections::HashMap;
+use std::array;
 
-use once_cell::sync::Lazy;
+use static_init::dynamic;
 
 use crate::{hart::Hart, trap::Exception, mmu::MMU};
 
@@ -16,10 +16,12 @@ pub(crate) enum InstructionWithType {
   L32(u32), L16(u16),
 }
 
-#[allow(clippy::type_complexity)]
-//                               opcode, (mask, comp, instructor)
-static INSTRUCTORS: Lazy<HashMap<u8, Vec<(u32, u32, Instructor)>>> = Lazy::new(|| {
-  let mut res: HashMap<u8, Vec<(u32, u32, Instructor)>> = HashMap::new();
+//                         (mask, comp, instructor), index is opcode
+type InstructionMap = [Vec<(u32, u32, Instructor)>; 128];
+
+#[dynamic]
+static INSTRUCTORS: InstructionMap = {
+  let mut res: InstructionMap = array::from_fn(|_| vec![]);
   let mut instructors: Vec<Instructor> = Vec::new();
   instructors.extend(i());
   instructors.extend(zifenci());
@@ -30,11 +32,11 @@ static INSTRUCTORS: Lazy<HashMap<u8, Vec<(u32, u32, Instructor)>>> = Lazy::new(|
   instructors.extend(d());
   instructors.extend(sm());
   for instructor in instructors {
-    res.entry(instructor.opcode).or_default()
+    res.get_mut(instructor.opcode).unwrap()
       .push((instructor.mask(), instructor.comp(), instructor));
   }
   res
-});
+};
 
 #[derive(Debug)]
 pub(crate) struct InstructionSegment {
@@ -47,7 +49,7 @@ pub(crate) struct InstructionSegment {
 pub(crate) struct Instructor {
   #[allow(dead_code)]
   pub(crate) name: &'static str,
-  pub(crate) opcode: u8,
+  pub(crate) opcode: usize,
   pub(crate) segments: Vec<InstructionSegment>,
   pub(crate) run: fn(inst: u32, len: InstructionLen, mmu: &mut MMU, hart: &mut Hart) -> Result<(), Exception>
 }
@@ -71,7 +73,11 @@ impl Instructor {
 }
 
 pub(crate) fn parse(inst: u32) -> Option<&'static Instructor> {
-  let (_, _, instructor) = INSTRUCTORS.get(&((inst & 0b1111111) as u8))?
-    .iter().find(|(mask, comp, _)| (inst & *mask) == *comp)?;
-  Some(instructor)
+  let set = INSTRUCTORS.get((inst & 0b1111111) as usize).unwrap();
+  for (mask, comp, instructor) in set {
+    if (inst & *mask) == *comp {
+      return Some(instructor);
+    }
+  }
+  None
 }
